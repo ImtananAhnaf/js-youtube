a3dcf54ba44b9189b84ba96141aab920c08fe709
Imtanan Ahnaf <bc200401263@vu.edu.pk>
2025-10-03 19:34:57 +0000

    TIME

--- 01_basics/05_datesinJS.js
+++ 01_basics/05_datesinJS.js
@@ -8,4 +8,16 @@ console.log(d.toDateString());//shows date only (no time)
 console.log(d.toISOString());//to ISO standard (used in APIs and databses)
 console.log(d.toJSON());//same as ISO but used when want to convert an object into JSON
 console.log(d.toLocaleDateString());
+console.log(typeof myDate);
+console.log(myDate.constructor.name);
+let myCreatedDate = new Date(2023, 0, 23 , 5, 3)
+console.log(myCreatedDate.toString());
+console.log(myCreatedDate.toLocaleString());
+let myTimeStamp=Date.now();
+let customEpoch = new Date(2003,5,19).getTime()
+let d1 = new Date(2025, 10, 4).getTime()
+let diff = d1 - customEpoch
+
+console.log(Math.floor(diff/1000/60/60/24/365));//to get year
+
 
